cfa7690759c1a24f110490a54428257f4eeebeab
dinhanhquoc <anhquoc004@gmail.com>
2024-01-20 19:38:25 +0700

    Lap1-Bai2-HoanThanh
    Đã hoàn thành bài 2

--- LAB1/src/tiil/edu/Main.java
+++ LAB1/src/tiil/edu/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void BaiTap1() {
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("Họ và tên: ");
 		String hoTen = scanner.nextLine();
@@ -16,7 +16,37 @@ public class Main {
 		System.out.println(" Điểm TB: "+diemTB);
 		System.out.println("-------------------");
 		System.out.printf("Họ và tên: %s%n", new Object[] { hoTen });
-		scanner.close();
+		scanner.close();	
+	}
+	//Hàm tính toán số liệu của bài tập 2
+	 	private static double calculateChuVi(double dai, double rong) {
+	        return (dai + rong) * 2;
+	    }    
+	    private static double calculateDienTich(double dai, double rong) {
+	        return dai * rong;
+	    }  
+	    private static double calculateCanhNhoNhat(double dai, double rong) {
+	        return Math.min(dai, rong);
+	    }
+	public static void BaiTap2() {
+			Scanner scanner = new Scanner(System.in);
+	        System.out.print("Nhập chiều dài của hình chữ nhật: ");
+	        double dai = scanner.nextDouble();
+	        System.out.print("Nhập chiều rộng của hình chữ nhật: ");
+	        double rong = scanner.nextDouble();
+	        double chuVi = calculateChuVi(dai, rong);
+	        double dienTich = calculateDienTich(dai, rong);
+	        double canhNhoNhat = calculateCanhNhoNhat(dai, rong);
+	        System.out.println("Chu vi: " + chuVi);
+	        System.out.println("Diện tích: " + dienTich);
+	        System.out.println("Cạnh nhỏ nhất: " + canhNhoNhat);
+	        scanner.close();
+		
+		
+	}
+	public static void main(String[] args) {
+		//BaiTap1();
+		BaiTap2();
 	}
 
 }
