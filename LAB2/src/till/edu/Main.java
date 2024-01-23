d9225798e4fd1e7fe4c2fd0e673859c164f6fd96
dinhanhquoc <anhquoc004@gmail.com>
2024-01-23 16:17:52 +0700

    UpdateBaiTap2
    Hoàn Thành Bài Tập 2

--- LAB2/src/till/edu/Main.java
+++ LAB2/src/till/edu/Main.java
@@ -32,9 +32,19 @@ public class Main {
 	        scanner.close();
 
 	 }
+	 public static void BaiTap2() {
+	 // Xuất bảng cửu chương từ 1 đến 9
+     for (int i = 1; i <= 9; i++) {
+         System.out.println("Bảng cửu chương " + i + ":");
+         for (int j = 1; j <= 10; j++) {
+        	  System.out.println(i + " x " + j + " = " + (i * j));
+         }
+         System.out.println();
+     }
+	 }
 
 	public static void main(String[] args) {
-		BaiTap1();
+		BaiTap2();
 	}
 
 }
